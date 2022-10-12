118271e4a2e74bbf2e6124c3c26d5918426055a6
ThugMS <leems041625@gmail.com>
2022-10-12 12:19:59 +0900

    [Feat] : 1010_월요일 코드 추가

--- 1005_수요일.cpp
+++ 1005_수요일.cpp
@@ -1,46 +1,46 @@
-#include <vgl.h>
-#include <InitShader.h>
-#include <vec.h>
-#include "MyColorCube.h"
-
-float gTime = 0.0f;
-GLuint prog;
-MyColorCube cube;
-
-void init()
-{
-	cube.init();
-	prog = InitShader("vShader_CubeRotation.glsl", "fShader_Cube.glsl");
-	glUseProgram(prog);
-}
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_DEPTH_TEST);
-	cube.draw(prog, gTime);
-	glFlush();
-}
-
-void myIdle()
-{
-	gTime += 0.001f;
-	glutPostRedisplay();
-}
-
-int main(int argc, char** argv)
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowSize(512, 512);
-	glutCreateWindow("Hello GL");
-
-	glewExperimental = true;
-	glewInit();
-
-	init();
-	glutIdleFunc(myIdle);
-	glutDisplayFunc(display);
-	glutMainLoop();
-
-	return 0;
-}
+//#include <vgl.h>
+//#include <InitShader.h>
+//#include <vec.h>
+//#include "MyColorCube.h"
+//
+//float gTime = 0.0f;
+//GLuint prog;
+//MyColorCube cube;
+//
+//void init()
+//{
+//	cube.init();
+//	prog = InitShader("vShader_CubeRotation.glsl", "fShader_Cube.glsl");
+//	glUseProgram(prog);
+//}
+//void display()
+//{
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//	glEnable(GL_DEPTH_TEST);
+//	cube.draw(prog, gTime);
+//	glFlush();
+//}
+//
+//void myIdle()
+//{
+//	gTime += 0.001f;
+//	glutPostRedisplay();
+//}
+//
+//int main(int argc, char** argv)
+//{
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+//	glutInitWindowSize(512, 512);
+//	glutCreateWindow("Hello GL");
+//
+//	glewExperimental = true;
+//	glewInit();
+//
+//	init();
+//	glutIdleFunc(myIdle);
+//	glutDisplayFunc(display);
+//	glutMainLoop();
+//
+//	return 0;
+//}
