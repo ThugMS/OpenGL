e932f03b3271863f1a480411ef3e1278fb4d9744
ThugMS <leems041625@gmail.com>
2022-11-30 13:11:29 +0900

    [Feat] : 11월 30일 수업

--- 1128 수업/Project1/Project1/fPhong.glsl
+++ 1128 수업/Project1/Project1/fPhong.glsl
@@ -14,6 +14,9 @@ uniform vec4 uDif;
 uniform vec4 uSpc; 
 uniform float uShininess; 
 uniform sampler2D uTex;
+uniform sampler2D uTexCld;
+uniform sampler2D uTexSpec;
+uniform sampler2D uTexLight;
 
 uniform float uTime;
 
@@ -27,11 +30,24 @@ void main()
     float NL = max(dot(N, L), 0); 
 	float VR = pow(max(dot(H, N), 0), uShininess); 
 	vec4 texColor = texture2D(uTex, TexCoord);
+	float cloud = texture2D(uTexCld, TexCoord - vec2(uTime/20,0)).r;  //구름을 확률로 보고 0~1사이의 숫자를 가져오기 위함
+	float spec = texture2D(uTexSpec, TexCoord).r;
+	vec4 lightColor = texture2D(uTexLight, TexCoord);
+
+
+	texColor += (1-cloud)*texColor + cloud*vec4(1,1,1,1); //색의 확률에 따라서 섞어서 사용하는거임
+	
+	
+	float NNL = max(dot(-N,L), 0) * 1.5;
+	lightColor = (1-cloud)*lightColor + cloud*vec4(0,0,0,1);
+
+
 
 	//fColor = uAmb + uDif*NL + uSpc*VR; 
 	//fColor = fColor * texColor;
 
-	fColor = uAmb + texColor*NL + uSpc*VR;
+	fColor = uAmb + texColor*NL + lightColor*NNL + uSpc*VR*spec;
+	fColor += pow((1 -max(dot(V,N), 0)), 5) * vec4(0.5,0.5,1,1);			//내가 바라보는 방향과 NORMAL이 0일 수록 경계면인 거임
 
 	fColor.w = 1;
 	
